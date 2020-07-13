6e6e620f237d83cc57bb5ab814f3d985cd015552
Yahya <yahyatinani@gmail.com>
2020-07-13 09:01:53 +0100

    Implement curried version of compose function

--- src/Lambdas.kt
+++ src/Lambdas.kt
@@ -2,6 +2,8 @@ import Conversion.*
 
 typealias DoubleConversion = (Double) -> Double
 
+fun <E> rest(): (Collection<E>) -> List<E> = { it.drop(1) }
+
 enum class Conversion {
     CentigradeToFahrenheit,
     KgsToPounds,
@@ -27,6 +29,30 @@ fun getConversionLambda(conversion: Conversion): DoubleConversion {
 fun combine(lambda1: DoubleConversion, lambda2: DoubleConversion): DoubleConversion =
     { x: Double -> lambda2(lambda1(x)) }
 
+val square = { n: Int -> n * n }
+
+val triple = { n: Int -> n * 3 }
+
+fun <T, U, V> compose(f: (U) -> V, g: (T) -> U): (T) -> V = { T -> f(g(T)) }
+
+val squareOfTriple = compose(square, triple)
+
+val add: (Int) -> IntToInt = { a -> { b -> a + b } }
+
+typealias IntToInt = (Int) -> Int
+
+fun <T, U, V> composeV3(): ((U) -> V) -> (((T) -> U) -> ((T) -> V)) =
+    { x ->
+        { y ->
+            { z ->
+                x(y(z))
+            }
+        }
+    }
+
+val composeV2: (IntToInt) -> (IntToInt) -> IntToInt =
+    { x -> { y -> { z -> x(y(z)) } } }
+
 fun main() {
     println("2.5 Kgs to pounds = ${getConversionLambda(KgsToPounds)(2.5)}")
 
@@ -38,4 +64,8 @@ fun main() {
     val v = 17.4
 
     println("$v Kgs to US Tons = ${convert(v, kgsToUsTonsLambdas)}")
+
+    println(rest<Int>()(listOf(1, 5, 6, 47, 2, 6, 3, 4)))
+
+    println("squareOfTriple = ${squareOfTriple(3)}")
 }
