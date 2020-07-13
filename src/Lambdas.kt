9e2bad286791eeb0ba879e20813d25a588a601eb
Yahya <yahyatinani@gmail.com>
2020-07-13 16:20:59 +0100

    Practice currying

--- src/Lambdas.kt
+++ src/Lambdas.kt
@@ -1,4 +1,5 @@
 import Conversion.*
+import java.lang.IllegalArgumentException
 
 typealias DoubleConversion = (Double) -> Double
 
@@ -42,16 +43,99 @@ val add: (Int) -> IntToInt = { a -> { b -> a + b } }
 typealias IntToInt = (Int) -> Int
 
 fun <T, U, V> composeV3(): ((U) -> V) -> (((T) -> U) -> ((T) -> V)) =
-    { x ->
-        { y ->
-            { z ->
-                x(y(z))
+    { f ->
+        { g ->
+            { x ->
+                f(g(x))
+            }
+        }
+    }
+
+fun <T, U, V> higherAndThen(): ((T) -> U) -> (((U) -> V) -> ((T) -> V)) =
+    { f ->
+        { g ->
+            { x ->
+                g(f(x))
             }
         }
     }
 
 val composeV2: (IntToInt) -> (IntToInt) -> IntToInt =
     { x -> { y -> { z -> x(y(z)) } } }
+
+val addTax = { tax: Double ->
+    { price: Double ->
+        price + price * tax
+    }
+}
+
+val tc5 = addTax(5.0)
+
+fun <A, B, C> partialA(a: A, f: (A) -> (B) -> C): (B) -> C = f(a)
+
+fun <A, B, C> partialB(b: B, f: (A) -> (B) -> C): (A) -> C = { a: A -> f(a)(b) }
+
+fun <A, B, C, D> func(a: A, b: B, c: C, d: D): String = "$a $b $c $d"
+
+fun <A, B, C, D> funCurried(): (A) -> (B) -> (C) -> (D) -> String =
+    { a ->
+        { b ->
+            { c ->
+                { d ->
+                    func(a, b, c, d)
+                }
+            }
+        }
+    }
+
+fun <A, B, C> curry(f: (A, B) -> C): (A) -> (B) -> C =
+    { a: A ->
+        { b: B ->
+            f(a, b)
+        }
+    }
+
+fun <T, U, V> taxOnly(f: (T) -> (U) -> V): (U) -> (T) -> V =
+    { u: U ->
+        { t: T ->
+            f(t)(u)
+        }
+    }
+
+data class Price private constructor(private val value: Double) {
+    override fun toString(): String = value.toString()
+
+    operator fun plus(price: Price) = Price(this.value + price.value)
+
+    operator fun times(n: Int) = Price(this.value * n)
+
+    companion object {
+        val identity = Price(0.0)
+
+        operator fun invoke(value: Double) =
+            if (value > 0)
+                Price(value)
+            else
+                throw IllegalArgumentException("Price must be Positive")
+
+    }
+}
+
+data class Weight(val value: Double) {
+    operator fun plus(weight: Weight) = Weight(this.value + weight.value)
+}
+
+val priceAddition = { x: Price, y: Price -> x + y }
+val weightAddition = { x: Weight, y: Weight -> x + y }
+
+val zeroPrice = Price.identity
+val zeroWeight = Weight(0.0)
+
+fun sumByPrice(prices: List<Price>) =
+    prices.fold(zeroPrice, priceAddition)
+
+fun sumByWeight(weights: List<Weight>) =
+    weights.fold(zeroWeight, weightAddition)
 
 fun main() {
     println("2.5 Kgs to pounds = ${getConversionLambda(KgsToPounds)(2.5)}")
